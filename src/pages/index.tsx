069c1fd9ffbb50231679e081066f1c2a578e9d96
David Girgoryan <dav71231@gmail.com>
2020-12-29 15:48:44 +0400

    Add lint staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Main: React.FC = () => {
   return (
     <div>
-      <h1>React + NextJS + TypeScript</h1>
+      <h1>Hello React + NextJS + TypeScript</h1>
     </div>
   );
 };
